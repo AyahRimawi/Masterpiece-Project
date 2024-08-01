bf74c44c3b49d5189dcf218197faa328c2117eb6
ayahrimawi <ayaalrimawi406@gmail.com>
2024-08-02 00:25:09 +0300

    Masterpiece-Project

--- masterpiece/src/pages/ProductPage.jsx
+++ masterpiece/src/pages/ProductPage.jsx
@@ -5,10 +5,10 @@ import FooterBar from "../components/Footer/FooterBar";
 
 const ProductPage = () => {
   const [images, setImages] = useState({
-    img1: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,b_rgb:f5f5f5/3396ee3c-08cc-4ada-baa9-655af12e3120/scarpa-da-running-su-strada-invincible-3-xk5gLh.png",
-    img2: "https://static.nike.com/a/images/f_auto,b_rgb:f5f5f5,w_440/e44d151a-e27a-4f7b-8650-68bc2e8cd37e/scarpa-da-running-su-strada-invincible-3-xk5gLh.png",
-    img3: "https://static.nike.com/a/images/f_auto,b_rgb:f5f5f5,w_440/44fc74b6-0553-4eef-a0cc-db4f815c9450/scarpa-da-running-su-strada-invincible-3-xk5gLh.png",
-    img4: "https://static.nike.com/a/images/f_auto,b_rgb:f5f5f5,w_440/d3eb254d-0901-4158-956a-4610180545e5/scarpa-da-running-su-strada-invincible-3-xk5gLh.png",
+    img1: "https://i.imgur.com/6xGVorA_d.webp?maxwidth=760&fidelity=grand",
+    img2: "https://i.imgur.com/0qaR8Fj_d.webp?maxwidth=760&fidelity=grand",
+    img3: "https://i.imgur.com/ZkZmF2C_d.webp?maxwidth=760&fidelity=grand",
+    // img4: "https://static.nike.com/a/images/f_auto,b_rgb:f5f5f5,w_440/d3eb254d-0901-4158-956a-4610180545e5/scarpa-da-running-su-strada-invincible-3-xk5gLh.png",
   });
 
   const [activeImg, setActiveImage] = useState(images.img1);
@@ -16,83 +16,82 @@ const ProductPage = () => {
   const [amount, setAmount] = useState(1);
 
   return (
-      <>
-          <TrackOrder />
-          <SearchBar/>
-      <div className="flex flex-col justify-between lg:flex-row gap-16 lg:items-center">
-        <div className="flex flex-col gap-6 lg:w-2/4">
+    <>
+      <TrackOrder />
+      <SearchBar />
+      <div className="flex flex-col justify-between lg:flex-row gap-16 lg:items-center md:mr-16 ml-16 mt-8 mb-8">
+        <div className="flex flex-col gap-4 lg:w-2/4">
           <img
             src={activeImg}
             alt=""
-            className="w-full h-full aspect-square object-cover rounded-xl"
+            className="w-full h-full aspect-square object-fit rounded-xl"
           />
           <div className="flex flex-row justify-between h-24">
             <img
               src={images.img1}
               alt=""
-              className="w-24 h-24 rounded-md cursor-pointer"
+              className="w-24 h-24 rounded-md cursor-pointer object-fit"
               onClick={() => setActiveImage(images.img1)}
             />
             <img
               src={images.img2}
               alt=""
-              className="w-24 h-24 rounded-md cursor-pointer"
+              className="w-24 h-24 rounded-md cursor-pointer object-fit"
               onClick={() => setActiveImage(images.img2)}
             />
             <img
               src={images.img3}
               alt=""
-              className="w-24 h-24 rounded-md cursor-pointer"
+              className="w-24 h-24 rounded-md cursor-pointer object-fit"
               onClick={() => setActiveImage(images.img3)}
             />
-            <img
+            {/* <img
               src={images.img4}
               alt=""
-              className="w-24 h-24 rounded-md cursor-pointer"
+              className="w-24 h-24 rounded-md cursor-pointer object-fit"
               onClick={() => setActiveImage(images.img4)}
-            />
+            /> */}
           </div>
         </div>
         {/* ABOUT */}
-        <div className="flex flex-col gap-4 lg:w-2/4">
+        <div className="flex flex-col gap-6 lg:w-2/4">
           <div>
-            <span className=" text-violet-600 font-semibold">
-              Special Sneaker
+            <span className=" text-[#193db0] font-semibold">
+              Floral Print Skirt
             </span>
-            <h1 className="text-3xl font-bold">Nike Invincible 3</h1>
+            <h1 className="text-3xl font-bold">Rose Skirt</h1>
           </div>
           <p className="text-gray-700">
-            Con un'ammortizzazione incredibile per sostenerti in tutti i tuoi
-            chilometri, Invincible 3 offre un livello di comfort elevatissimo
-            sotto il piede per aiutarti a dare il massimo oggi, domani e oltre.
-            Questo modello incredibilmente elastico e sostenitivo, è pensato per
-            dare il massimo lungo il tuo percorso preferito e fare ritorno a
-            casa carico di energia, in attesa della prossima corsa.
+            Elevate your wardrobe with our charming Floral Print Skirt. Crafted
+            from high-quality, breathable fabric, this skirt combines elegance
+            with comfort. Its vibrant floral pattern adds a touch of playful
+            sophistication, making it perfect for both casual outings and
+            special occasions.
           </p>
-          <h6 className="text-2xl font-semibold">$ 199.00</h6>
+          <h6 className="text-2xl font-semibold">JD 17.99</h6>
           <div className="flex flex-row items-center gap-12">
             <div className="flex flex-row items-center">
               <button
-                className="bg-gray-200 py-2 px-5 rounded-lg text-violet-800 text-3xl"
+                className="bg-gray-200 py-2 px-5 rounded-lg text-[#193db0] text-3xl"
                 onClick={() => setAmount((prev) => prev - 1)}
               >
                 -
               </button>
               <span className="py-4 px-6 rounded-lg">{amount}</span>
               <button
-                className="bg-gray-200 py-2 px-4 rounded-lg text-violet-800 text-3xl"
+                className="bg-gray-200 py-2 px-4 rounded-lg text-[#193db0] text-3xl"
                 onClick={() => setAmount((prev) => prev + 1)}
               >
                 +
               </button>
             </div>
-            <button className="bg-violet-800 text-white font-semibold py-3 px-16 rounded-xl h-full">
+            <button className="bg-[#193db0] text-white font-semibold py-3 px-16 rounded-xl h-full">
               Add to Cart
             </button>
           </div>
         </div>
-          </div>
-          <FooterBar/>
+      </div>
+      <FooterBar />
     </>
   );
 };
